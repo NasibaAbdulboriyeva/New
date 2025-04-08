6d8234ab1273beac0c35b96d0d53963c8c03903a
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-04-08 20:25:59 +0500

    compare stringVSstBuilder

--- New/Program.cs
+++ New/Program.cs
@@ -1,23 +1,31 @@
-﻿namespace New
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Diagnostics;
+using System.Text;
 
+class Program
+{
+    static void Main()
+    {
+        int count = 10000;
+
+        // STRING bilan test
+        Stopwatch sw1 = Stopwatch.StartNew();
+        string result = "";
+        for (int i = 0; i < count; i++)
+        {
+            result += "Salom";
         }
-      
+        sw1.Stop();
+        Console.WriteLine("string: " + sw1.ElapsedMilliseconds + " ms");
+
+        // STRINGBUILDER bilan test
+        Stopwatch sw2 = Stopwatch.StartNew();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            sb.Append("Salom");
+        }
+        sw2.Stop();
+        Console.WriteLine("StringBuilder: " + sw2.ElapsedMilliseconds + " ms");
     }
 }
